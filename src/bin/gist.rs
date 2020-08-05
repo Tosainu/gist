3b2e6d73e1c076e81011f656dcba5905a3f47422
Kenta Sato <tosainu.maple@gmail.com>
2020-08-05 21:02:26 +0900

    Config file

--- src/bin/gist.rs
+++ src/bin/gist.rs
@@ -1,14 +1,22 @@
+use std::collections::BTreeMap;
+use std::fs::File;
+use std::io::BufReader;
 use std::path::PathBuf;
 
+use anyhow::anyhow;
 use structopt::StructOpt;
+
+type Username = String;
+type Token = String;
+type Config = BTreeMap<Username, Token>;
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "simple GitHub Gist CLI")]
 struct Args {
-    #[structopt(short, required = true)]
-    user: String,
-    #[structopt(short, required = true)]
-    token: String,
+    #[structopt(short)]
+    user: Option<String>,
+    #[structopt(short, requires = "user")]
+    token: Option<String>,
     #[structopt(short)]
     secret: bool,
     #[structopt(short)]
@@ -20,13 +28,35 @@ struct Args {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::from_args();
 
+    let (user, token) = match (args.user, args.token) {
+        (Some(u), Some(t)) => Ok((u, t)),
+        (Some(u), None) => load_config()?
+            .remove_entry(&u)
+            .ok_or_else(|| anyhow!("token for '{}' not found", u)),
+        _ => load_config()?
+            .into_iter()
+            .next()
+            .ok_or_else(|| anyhow!("empty config file")),
+    }?;
+
     gist::post(
-        &args.user,
-        &args.token,
+        &user,
+        &token,
         !args.secret,
         args.description.as_deref(),
         &args.files,
     )?;
 
     Ok(())
+}
+
+fn load_config() -> Result<Config, Box<dyn std::error::Error>> {
+    let config_dir =
+        dirs::config_dir().ok_or_else(|| anyhow!("couldn't find the configuration directory"))?;
+    let config_file = config_dir.join("gist").join("config.json");
+
+    let file = File::open(config_file)?;
+    let reader = BufReader::new(file);
+
+    Ok(serde_json::from_reader(reader)?)
 }
