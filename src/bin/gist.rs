13abfb65d213515f0a86a0b761f90cdec35b2fe8
Kenta Sato <tosainu.maple@gmail.com>
2020-08-08 14:46:01 +0900

    Move account-related options to top-level

--- src/bin/gist.rs
+++ src/bin/gist.rs
@@ -11,17 +11,42 @@ type Config = BTreeMap<Username, gist::Login>;
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "simple GitHub Gist CLI")]
-enum Args {
+struct Args {
+    #[structopt(flatten)]
+    account: Account,
+
+    #[structopt(subcommand)]
+    command: Subcommand,
+}
+
+#[derive(Debug, StructOpt)]
+struct Account {
+    /// Specify OAuth2 access token
+    #[structopt(short, conflicts_with_all = &["user-and-token", "config"])]
+    access_token: Option<String>,
+
+    /// Specify user name and personal access token
+    #[structopt(
+        short,
+        conflicts_with_all = &["access-token", "config"],
+        parse(try_from_str = parse_user_and_token),
+        value_name = "user>:<token"
+    )]
+    user_and_token: Option<(String, String)>,
+
+    /// Use saved token
+    #[structopt(short, conflicts_with_all = &["access-token", "user-and-token"])]
+    config: Option<String>,
+}
+
+#[derive(Debug, StructOpt)]
+enum Subcommand {
     Login,
     Upload(Upload),
 }
 
 #[derive(Debug, StructOpt)]
 struct Upload {
-    #[structopt(short)]
-    user: Option<String>,
-    #[structopt(short, requires = "user")]
-    token: Option<String>,
     #[structopt(short)]
     secret: bool,
     #[structopt(short)]
@@ -30,28 +55,50 @@ struct Upload {
     files: Vec<PathBuf>,
 }
 
+fn parse_user_and_token(s: &str) -> Result<(String, String), Box<dyn std::error::Error>> {
+    let pos = s
+        .find(':')
+        .ok_or_else(|| anyhow!("no ':' found in '{}'", s))?;
+    Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::from_args();
 
-    match args {
-        Args::Login => login(),
-        Args::Upload(u) => upload(u),
+    match args.command {
+        Subcommand::Login => login(),
+        Subcommand::Upload(u) => {
+            let l = select_account(args.account)?;
+            upload(&l, u)
+        }
     }
 }
 
-fn upload(args: Upload) -> Result<(), Box<dyn std::error::Error>> {
-    let login = match (args.user, args.token) {
-        (Some(user), Some(token)) => Ok(gist::Login::PersonalAccessToken { user, token }),
-        (Some(u), None) => load_config()?
-            .remove(&u)
-            .ok_or_else(|| anyhow!("token for '{}' not found", u)),
-        _ => load_config()?
+fn select_account(account: Account) -> Result<gist::Login, Box<dyn std::error::Error>> {
+    if let Some(token) = account.access_token {
+        return Ok(gist::Login::OAuth(token));
+    }
+
+    if let Some((user, token)) = account.user_and_token {
+        return Ok(gist::Login::PersonalAccessToken { user, token });
+    }
+
+    let login = if let Some(key) = account.config {
+        load_config()?
+            .remove(&key)
+            .ok_or_else(|| anyhow!("token for '{}' not found", key))
+    } else {
+        load_config()?
             .into_iter()
             .next()
             .map(|l| l.1)
-            .ok_or_else(|| anyhow!("empty config file")),
+            .ok_or_else(|| anyhow!("empty config file"))
     }?;
 
+    Ok(login)
+}
+
+fn upload(login: &gist::Login, args: Upload) -> Result<(), Box<dyn std::error::Error>> {
     gist::upload(
         &login,
         !args.secret,
